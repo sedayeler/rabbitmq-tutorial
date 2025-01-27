f312951048d9cb777a7bc39e3655015a42918fb1
Seda Yeler <sedaayeler@gmail.com>
2025-01-27 21:48:12 +0300

    Commit

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://uilltroy:fF8_cjK6zL2ia3G5S3NIUQurkhQsx7yj@jackal.rmq.cloudamqp.com/uilltroy");
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "example", durable: true, exclusive: false);
+
+AsyncEventingBasicConsumer consumer = new(channel);
+await channel.BasicConsumeAsync(queue: "example", autoAck: false, consumer: consumer);
+consumer.ReceivedAsync += async (sender, e) =>
+{
+    //Kuyruğa gelen mesajın işlendiği yerdir.
+    //e.Body : Kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+    //e.Body.Span veya e.Body.ToArray() : Kuyruktaki mesajın byte verisini getirecektir.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    await channel.BasicAckAsync(deliveryTag: e.DeliveryTag, multiple: false);
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://uilltroy:fF8_cjK6zL2ia3G5S3NIUQurkhQsx7yj@jackal.rmq.cloudamqp.com/uilltroy");
+
+//Bağlantıyı aktifleştirme ve kanal açma
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+//Queue oluşturma
+await channel.QueueDeclareAsync(queue: "example", durable: true, exclusive: false);
+
+//Queue'ya mesaj gönderme
+//RabbitMQ kuyruktaki mesajları byte türünden kabul etmektedir. 
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+await channel.BasicPublishAsync(exchange: "", routingKey: "example", body: message);
+
+IBasicProperties properties = new BasicProperties();
+properties.Persistent = true;
+
+Console.Read();
+
