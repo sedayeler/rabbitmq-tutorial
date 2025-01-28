c51be5a4af1818aed44a74a3b475e145c4449ef8
Seda Yeler <sedaayeler@gmail.com>
2025-01-28 18:50:41 +0300

    Topic Exchange

---
+++ RabbitMQ.TopicExchange.Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://uilltroy:fF8_cjK6zL2ia3G5S3NIUQurkhQsx7yj@jackal.rmq.cloudamqp.com/uilltroy");
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+Console.Write("Topic giriniz: ");
+string topic = Console.ReadLine();
+
+var queue = await channel.QueueDeclareAsync();
+string queueName = queue.QueueName;
+
+await channel.QueueBindAsync(queue: queueName, exchange: "topic-exchange-example", routingKey: topic);
+
+AsyncEventingBasicConsumer consumer = new(channel);
+await channel.BasicConsumeAsync(queue: queueName, autoAck: false, consumer: consumer);
+
+consumer.ReceivedAsync += async (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+    await channel.BasicAckAsync(deliveryTag: e.DeliveryTag, multiple: false);
+};
+
+Console.Read();
---
+++ RabbitMQ.TopicExchange.Publisher/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://uilltroy:fF8_cjK6zL2ia3G5S3NIUQurkhQsx7yj@jackal.rmq.cloudamqp.com/uilltroy");
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+for (int i = 0; i < 50; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    Console.Write("Topic giriniz: ");
+    string topic = Console.ReadLine();
+
+    await channel.BasicPublishAsync(exchange: "topic-exchange-example", routingKey: topic, body: message);
+}
